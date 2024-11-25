64c140acdb2691ee5eca2b25d89c57be48369756
Baptiste Desharbes <desharbesbaptiste@hotmail.fr>
2024-11-25 12:29:18 +0100

    I have done some changes on app.js and path of posters

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-require("dotenv").config();
+const express = require("express");
 const jsonServer = require("json-server");
 const morgan = require("morgan");
+const path = require("path");
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 5005;
 
+// Servir les fichiers statiques depuis le dossier "public/images/posters"
+server.use(
+  "/images/posters",
+  express.static(path.join(__dirname, "public", "images", "posters"))
+);
+
 server.use(middlewares);
 server.use(morgan("dev"));
+
+// Middleware pour désactiver CORS
 server.use((req, res, next) => {
-  // Middleware to disable CORS
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
+
 server.use(router);
 
 server.listen(PORT, () => {
